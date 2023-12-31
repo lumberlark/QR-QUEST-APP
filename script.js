f7d9cf2aaa9bec28477c4c51cdf31d4ace1a07b3
lumberlark <slark20@gmail.com>
2023-12-30 23:04:12 -0600

    updated link file

--- script.js
+++ script.js
@@ -1,110 +1,114 @@
-const textE1 = document.querySelector('#data');
-const sizeE1 = document.querySelector('#size');
-const logoE1 = document.querySelector('#logo');
-const clearE1 = document.querySelector('#clear');
-const marginEl = document.querySelector('#margin');
-const dotModeEl = document.querySelector('#dot');
-const dotColorEl1 = document.querySelector('#dot-color-1');
-const dotColorEl2 = document.querySelector('#dot-color-2');
-const bgEl = document.querySelector('#bg-color');
-const dlEl = document.querySelector('#btn-dl');
+document.addEventListener("DOMContentLoaded", function () {
+  const textE1 = document.querySelector("#data");
+  const sizeE1 = document.querySelector("#size");
+  const logoE1 = document.querySelector("#logo");
+  const clearE1 = document.querySelector("#clear");
+  const marginEl = document.querySelector("#margin");
+  const dotModeEl = document.querySelector("#dot");
+  const dotColorEl1 = document.querySelector("#dot-color-1");
+  const dotColorEl2 = document.querySelector("#dot-color-2");
+  const bgEl = document.querySelector("#bg-color");
+  const dlEl = document.querySelector("#btn-dl");
+  const camera = document.querySelector("#camera");
 
-let op={
+  let op = {
     width: 100,
     height: 100,
     type: "png",
     data: textE1.value,
-    image: "https://upload.wikimedia.org/wikipedia/commons/5/51/Facebook_f_logo_%282019%29.svg",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/5/51/Facebook_f_logo_%282019%29.svg",
     dotsOptions: {
-        color: "#4267b2",
-        type: "rounded",
-        gradient: {
-            "type" : "linear",
-            "colorStops": [
-                {
-                    "offset": 0,
-                    "color": "#000000"
-                },
-                {
-                    "offset": 1,
-                    "color": "#000"
-                }
-            ]
-        }
+      color: "#4267b2",
+      type: "rounded",
+      gradient: {
+        type: "linear",
+        colorStops: [
+          {
+            offset: 0,
+            color: "white",
+          },
+          {
+            offset: 1,
+            color: "white",
+          },
+        ],
+      },
     },
     backgroundOptions: {
-        color: "#fff",
-    }
-};
+      color: "salmon",
+    },
+  };
 
-render();
+  render();
 
-// Width and Height of QR code changed
-sizeE1.addEventListener('input', e=>{
+  // Width and Height of QR code changed
+  sizeE1.addEventListener("input", (e) => {
     op.width = e.target.value * 10;
     op.height = e.target.value * 10;
     render();
-})
+  });
 
-// QR code will change with text-area
-textE1.addEventListener('keyup', e=>{
+  // QR code will change with text-area
+  textE1.addEventListener("keyup", (e) => {
     op.data = e.target.value;
     render();
-});
+  });
 
-marginEl.addEventListener('input', e=>{
-    op.imageOptions = {margin: e.target.value};
+  marginEl.addEventListener("input", (e) => {
+    op.imageOptions = { margin: e.target.value };
     render();
-});
+  });
 
-dotModeEl.addEventListener('change', e=>{
+  dotModeEl.addEventListener("change", (e) => {
     op.dotsOptions.type = e.target.value;
     render();
-});
+  });
 
-dotColorEl1.addEventListener('input', e=>{
+  dotColorEl1.addEventListener("input", (e) => {
     op.dotsOptions.gradient.colorStops[0].color = e.target.value;
     render();
-});
-dotColorEl2.addEventListener('input', e=>{
+  });
+  dotColorEl2.addEventListener("input", (e) => {
     op.dotsOptions.gradient.colorStops[1].color = e.target.value;
     render();
-});
+  });
 
-bgEl.addEventListener('input', e=>{
+  bgEl.addEventListener("input", (e) => {
     op.backgroundOptions.color = e.target.value;
     render();
-})
+  });
 
-var qrCode;
-function render(){
+  var qrCode;
+  function render() {
     qrCode = new QRCodeStyling(op);
-    let canvasE1 = document.querySelector('#canvas');
-    canvasE1.innerHTML='';
+    let canvasE1 = document.querySelector("#canvas");
+    canvasE1.innerHTML = "";
     qrCode.append(canvasE1);
     canvasE1.nextElementSibling.innerHTML = `${op.width}px x ${op.height}px`;
-}
+  }
 
-function browse(){
-    logoE1.click();
-}
+  // allows browsing of files when clicking on logo or camera image
+  document.getElementById("cameraLabel").addEventListener("click", function () {
+    document.getElementById("logo").click();
+  });
 
-logoE1.addEventListener('change', e=>{
+  logoE1.addEventListener("change", (e) => {
     let file = e.target.files[0];
     let reader = new FileReader();
     reader.onload = () => {
-        op.image = reader.result;
-        render();
+      op.image = reader.result;
+      render();
     };
     reader.readAsDataURL(file);
-});
+  });
 
+  dlEl.addEventListener("click", (e) => {
+    qrCode.download({ name: "qr", extension: "svg" });
+  });
 
-dlEl.addEventListener('click', e=>{
-    qrCode.download({name:'qr', extension:'svg'})
-});
-
-clearE1.addEventListener('click', e=>{
+  clearE1.addEventListener("click", (e) => {
     delete op.image;
     render();
+  });
 });
